c9aacce61027724b019eee19e7331a34c8b1f13a
SamehRefaay <samehrefaay89@gmail.com>
2024-12-11 20:02:37 +0200

    adding footer to the website

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+// components
+import Logo from './Logo';
+
+const Footer = () => {
+	return (
+		<footer className="p-12 bg-primary">
+			<div className="container mx-auto">
+				<div className="flex flex-col xl:flex-row items-center justify-between">
+					<Logo light={true} />
+					<div className="text-white/70 font-light">
+						Copyright &copy; 2024. All rights reserved.
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
